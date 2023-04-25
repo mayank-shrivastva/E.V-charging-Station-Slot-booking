d031c638b307397b8eface2c376161ecc66e5880
VaibhavShakya26 <106857725+VaibhavShakya26@users.noreply.github.com>
2023-04-25 10:41:57 +0530

    Update services.php

--- services.php
+++ services.php
@@ -46,13 +46,13 @@
                 <div class="col-lg-6">
                     <h5 class="about-h5" data-aos="fade-left" data-aos-duration="1000" data-aos-mirror="true"><i class="fa-solid fa-bolt"></i>Electronic Vehicles Rental</h5>
                     <p class="services-page-p" data-aos="fade-up" data-aos-duration="1000" data-aos-mirror="true">The EV charging station and car booking project is a comprehensive
-solution designed to make it easier for people to transition to electric
-vehicles. The project includes the development of a platform that allows
-users to find and book electric cars in their area. The platform will be designed to be user-friendly and accessible, , even for
-people who may not be familiar with electric vehicles or the charging
-process. The platform will feature a map that shows the location of
-available charging stations and electric cars, along with real-time
-availability information.
+                            solution designed to make it easier for people to transition to electric
+                            vehicles. The project includes the development of a platform that allows
+                            users to find and book electric cars in their area. The platform will be designed to be user-friendly and accessible, , even for
+                            people who may not be familiar with electric vehicles or the charging
+                            process. The platform will feature a map that shows the location of
+                            available charging stations and electric cars, along with real-time
+                            availability information.
                     </p>
                 </div>
 </div>
@@ -60,12 +60,12 @@ availability information.
                 <div class="col-lg-6">
                     <h5 class="about-h5" data-aos="fade-right" data-aos-duration="1000" data-aos-mirror="true"><i class="fa-solid fa-bolt"></i>Electronic Vehicles Charging</h5>
                     <p class="services-page-p" data-aos="fade-up" data-aos-duration="1000" data-aos-mirror="true">Users will be able to reserve a charging station or an electric car directly
-from the platform, and receive notifications when their reservation is
-confirmed. The platform will also provide users with information on the
-estimated charging time and cost, as well as the distance that the electric car
-can travel on a single charge. In addition to the development of the platform, the project will also involve
-the installation of new EV charging stations in strategic locations, such as
-shopping malls, airports, and public parking areas.
+                            from the platform, and receive notifications when their reservation is
+                            confirmed. The platform will also provide users with information on the
+                            estimated charging time and cost, as well as the distance that the electric car
+                            can travel on a single charge. In addition to the development of the platform, the project will also involve
+                            the installation of new EV charging stations in strategic locations, such as
+                            shopping malls, airports, and public parking areas.
                     </p>
 </div>
                 <div class="col-lg-6" id="about-image">
